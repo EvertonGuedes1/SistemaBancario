ab5ec2cc62a7954cbc7654e3dc6c181671a9a80e
Everton Guedes <evertonguedes1@gmail.com>
2022-03-01 18:01:43 -0300

    validação das entradas das requisições

--- src/index.ts
+++ src/index.ts
@@ -9,13 +9,24 @@ app.use(cors());
 
 app.post("/users/create", (req: Request, res: Response) => {
   try {
-    // validar as entradas da requisição
-    // consultar ou alterar a base de dados
     const { name, CPF, dateOfBirthAsString } = req.body;
 
-    const [day, month, year] = dateOfBirthAsString.split("");
+    const [day, month, year] = dateOfBirthAsString.split("/");
 
     const dateOfBirth: Date = new Date(`${year}-${month}-${day}`);
+
+    // validar as entradas da requisição
+
+    const ageInMiliseconds: number = Date.now() - dateOfBirth.getTime();
+
+    const ageInYears: number = ageInMiliseconds / 1000 / 60 / 60 / 24 / 365;
+
+    if (ageInYears < 18) {
+      res.statusCode = 406;
+      throw new Error("Idade deve ser maior que 18 anos");
+    }
+
+    // consultar ou alterar a base de dados
 
     accounts.push({
       name,
@@ -27,10 +38,23 @@ app.post("/users/create", (req: Request, res: Response) => {
 
     // validar os resultados da consulta
     // enviar a resposta
-    res.status(201).send("COnta criada com sucesso!");
+    res.status(201).send("Conta criada com sucesso!");
   } catch (error: any) {
     console.log(error);
-    res.status(400).send(error.message);
+    res.send(error.message);
+  }
+});
+
+app.get("/users/all", (req: Request, res: Response) => {
+  try {
+    if (!accounts.length) {
+      res.statusCode = 404;
+      throw new Error("Nenhuma conta encontrada");
+    }
+
+    res.status(200).send(accounts);
+  } catch (error: any) {
+    res.send(error.message);
   }
 });
 
