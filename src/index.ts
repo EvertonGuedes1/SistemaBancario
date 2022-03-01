b6aa740e8e6fc6317e76c2bc5aa733fe697aaf40
Everton Guedes <evertonguedes1@gmail.com>
2022-03-01 11:57:42 -0300

    servidor configurado e pronto para criação dos endpoints

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-console.log("oi");
+import express, { Express, Request, Response } from "express";
+import cors from "cors";
+
+const app: Express = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.listen(3003, () => {
+  console.log("Servidor rodando na porta 3003");
+});
